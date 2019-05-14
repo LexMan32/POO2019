93a8f7cd70f14872b086dfbb3caf2e66dd504008
morealedivtec <alexandre.morel@divtec.ch>
2019-05-14 15:15:45 +0200

    Add MVCAppli + Debut config

---
+++ MVCAppli/Saisie2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static MVCAppli.App;
+
+namespace MVCAppli
+{
+    /// <summary>
+    /// Logique d'interaction pour Saisie2.xaml
+    /// </summary>
+    public partial class Saisie2 : Window
+    {
+        public SetTbSaisieDelegate setTbSaisieDelegateCallback;
+
+        public Saisie2()
+        {
+            InitializeComponent();
+        }
+
+        private void tbSaisie2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+    }
+}
